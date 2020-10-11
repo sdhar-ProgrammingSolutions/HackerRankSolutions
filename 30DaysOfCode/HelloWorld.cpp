6181a824948d2339c44d434b1f4c4db07ec481d9
sdhar-ProgrammingSolutions <sohinid1991@gmail.com>
2020-10-11 11:56:00 +0530

    Adding code

---
+++ 30DaysOfCode/HelloWorld.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() 
+{
+    // Declare a variable named 'input_string' to hold our input.
+    string input_string; 
+    
+    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+    getline(cin, input_string); 
+
+    cout << "Hello, World." << endl;
+
+    return 0;
+}
